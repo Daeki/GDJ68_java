2f7a5dc49b2033d6043abd4ed018f3b94e5d9d91
Daeki <kimdaeki@gmail.com>
2023-06-29 09:51:40 +0900

    0629-1

--- src/com/iu/study2/Study5_while_ex1.java
+++ src/com/iu/study2/Study5_while_ex1.java
@@ -30,46 +30,67 @@ public class Study5_while_ex1 {
 					System.out.println("로그인 실패");
 				}
 			}else {
+				//check=false;
+				check=!check;
 				break;
 			}
 		}//while 끝
 		
-		
-		//로그인 성공 했을 때만 진행
-		//MMORPG
-		//시작레벨:1
-		//모든 몬스터의 경험치는 동일
-		//최대레벨:15
-		//GOLD : 0
-		//5레벨 달성시 : 1000GOLD 지금
-		//10레벨 달성시 : 2000GOLD 지급
-		//15레벨 달성시 : 3000GOLD 지급
-		//1 -> 2 : 3 (사냥성공)
-		//2 -> 3 : 6
-		//3 -> 4 : 9
-		//4 -> 5 : 12
-		//...
-		//14 -> 15: 42
-		//랩업시 게임을 계속할지 종료할 지 선택
-		int level=1;
-		
-		for(level=1;level<15;level++) {
+		if(check) {
+			//로그인 성공 했을 때만 진행
+			//MMORPG
+			//시작레벨:1
+			//모든 몬스터의 경험치는 동일
+			//최대레벨:15
+			//GOLD : 0
+			//5레벨 달성시 : 1000GOLD 지금
+			//10레벨 달성시 : 2000GOLD 지급
+			//15레벨 달성시 : 3000GOLD 지급
+			//1 -> 2 : 3 (사냥성공)
+			//2 -> 3 : 6
+			//3 -> 4 : 9
+			//4 -> 5 : 12
+			//...
+			//14 -> 15: 42
+			//랩업시 게임을 계속할지 종료할 지 선택
+			int level=1;
+			int gold=0;
 			
-			//랩업시 게임을 계속할지 종료할 지 선택	
-			System.out.println("1.사냥시작 2.게임종료");
-			int select=sc.nextInt();
-			if(select !=1) {
-				break;
+			for(level=1;level<15;level++) {
+				
+				if(level%5==0) {
+					System.out.println(level+"랩 달성 축하 합니다");
+					gold = gold+ level/5*1000;
+				}
+	//			if(level==10) {
+	//				System.out.println("10랩 달성 축하 합니다");
+	//				gold=gold+2000;
+	//			}
+				
+				
+				//랩업시 게임을 계속할지 종료할 지 선택	
+				System.out.println("1.사냥시작 2.게임종료");
+				int select=sc.nextInt();
+				if(select !=1) {
+					break;
+				}
+				
+				for(int monster=0;monster<level*3;monster++) {
+					System.out.println(monster+1+ " 마리 사냥 성공");
+				}
+							
+				System.out.println(level+1+ "로 랩업 했습니다.");
+			}//레벨업 과정
+			
+			if(level==15) {
+				gold=gold+3000;
 			}
-						
-			System.out.println(level+1+ "로 랩업 했습니다.");
+			
+			//현재레벨, GOLD 가 출력
+			System.out.println("현재 레벨 : "+level);
+			System.out.println("현재 골드 : "+gold);
+		
 		}
-		
-		
-		
-		//현재레벨, GOLD 가 출력
-		
-		
 		
 		
 		System.out.println("프로그램 종료");
--- src/com/iu/study2/Study5_while_ex2.java
+++ src/com/iu/study2/Study5_while_ex2.java
@@ -1,6 +1,7 @@
 package com.iu.study2;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Study5_while_ex2 {
 
@@ -10,7 +11,8 @@ public class Study5_while_ex2 {
 		int hp=40;//유저의 HP
 		int monsterHp=60;//몬스터의 HP
 		int monsterPower=40;//몬스터의 공격력
-		
+					
+				
 		//유저정보 : hp, 몬스터 정보 :hp
 		//1.평타 2.스킬 3.HP회복 4.도망
 		//평타 : 0~10 미만의 공격이 몬스터에게 적용
@@ -20,7 +22,28 @@ public class Study5_while_ex2 {
 		
 		//monster
 		//1 : 유저 공격 0~40의 공격을 유저에게
-		
+		boolean flag=true;
+		Scanner sc = new Scanner(System.in);
+		while(flag) {
+			System.out.println("1.평타 2.스킬 3.회복 4.도망");
+			int select = sc.nextInt();
+			
+			if(select==1) {
+				//평타
+				//Damage
+				select = random.nextInt(11);
+				monsterHp=monsterHp-select;
+				System.out.println("몹에게 "+select+ " 피해를 주었습니다");
+			}else if(select==2) {
+				//스킬
+			}else if(select==3) {
+				//회복
+			}else {
+				//도망
+				break;
+			}
+			
+		}
 		
 		
 		
