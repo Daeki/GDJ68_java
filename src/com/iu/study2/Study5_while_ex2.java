9905f6814f34cbf313c17be66b97e4d3b391e7d6
Daeki <kimdaeki@gmail.com>
2023-06-29 10:20:50 +0900

    0629-2

--- src/com/iu/study2/Study5_while_ex2.java
+++ src/com/iu/study2/Study5_while_ex2.java
@@ -15,8 +15,8 @@ public class Study5_while_ex2 {
 				
 		//유저정보 : hp, 몬스터 정보 :hp
 		//1.평타 2.스킬 3.HP회복 4.도망
-		//평타 : 0~10 미만의 공격이 몬스터에게 적용
-		//스킬 : 0~30 미만의 공격이 몬스터에게 적용(최대3번까지만)
+		//평타 : 0~10 이하의 공격이 몬스터에게 적용
+		//스킬 : 0~30 이하의 공격이 몬스터에게 적용(최대3번까지만)
 		//회복 : 회복량 0~40 까지 40X
 		//도망 : 50%확률로 성공, 실패
 		
@@ -24,6 +24,7 @@ public class Study5_while_ex2 {
 		//1 : 유저 공격 0~40의 공격을 유저에게
 		boolean flag=true;
 		Scanner sc = new Scanner(System.in);
+		int skillCount=3;
 		while(flag) {
 			System.out.println("1.평타 2.스킬 3.회복 4.도망");
 			int select = sc.nextInt();
@@ -34,16 +35,53 @@ public class Study5_while_ex2 {
 				select = random.nextInt(11);
 				monsterHp=monsterHp-select;
 				System.out.println("몹에게 "+select+ " 피해를 주었습니다");
+				if(monsterHp<1) {
+					System.out.println("몹이 죽었어요");
+					break;
+				}
+				
 			}else if(select==2) {
 				//스킬
+				if(skillCount>0) {
+					select = random.nextInt(31);
+					monsterHp=monsterHp-select;
+					System.out.println("몹에게 "+select+ " 피해를 주었습니다");
+					if(monsterHp<1) {
+						System.out.println("몹이 죽었어요");
+						break;
+					}
+					skillCount--;
+				}else {
+					System.out.println("마나가 부족합니다");
+				}
 			}else if(select==3) {
 				//회복
+				select = random.nextInt(41);
+				hp=hp+select;
+				if(hp>40) {
+					hp=40;
+				}
+				System.out.println("Hp를 회복했어요");
 			}else {
-				//도망
+				select = random.nextInt(1001);
+				if(select%2 != 0) {
+					System.out.println("도망 성공");
+					break;
+				}else {
+					System.out.println("도망 실패");
+				}
+			}//유저의 끝
+			
+			select=random.nextInt(41);
+			hp=hp-select;
+			System.out.println("유저에게 "+select+ " 피해를 주었습니다");
+			if(hp<1) {
+				System.out.println("You Die");
 				break;
 			}
 			
-		}
+			
+		}//while 끝
 		
 		
 		
